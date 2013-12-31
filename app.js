0dcdaeaa99f9599fa1f08ef89cab4e7ac7c6ac1e
Brian Han <brianhan87usa@gmail.com>
2013-12-31 09:16:05 -0500

    Add dynamic route to link to indiviual product pages

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var App = Ember.Application.create({
 App.Router.map(function(){
   this.route('about');
   this.resource('products');
+  this.resource('product', { path: '/products/:title' });
   this.resource('contacts');
 });
 
