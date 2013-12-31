f9e43d3d55e58eb712aab872d9c00ca79fd62498
Brian Han <brianhan87usa@gmail.com>
2013-12-31 17:47:14 -0500

    Create associations 'Product hasMany Reviews' along with Review Model

--- app.js
+++ app.js
@@ -73,7 +73,13 @@ App.Product = DS.Model.extend({
   price: DS.attr('number'),
   description: DS.attr('string'),
   isOnSale: DS.attr('boolean'),
-  image: DS.attr('string')
+  image: DS.attr('string'),
+  reviews: DS.hasMany('review', {async: true})
+});
+
+App.Review = DS.Model.extend({
+  text: DS.attr('string'),
+  reviewedAt: DS.attr('date')
 });
 
 App.Contact = DS.Model.extend({
