64f9856b1a7f54ce125d24bcb47b0ca1d95d032d
Brian Han <brianhan87usa@gmail.com>
2013-12-31 17:13:43 -0500

    Change PRODUCTS to Product.FIXTURES with unique ids

--- app.js
+++ app.js
@@ -53,15 +53,17 @@ App.ContactsRoute = Ember.Route.extend({
   }
 });
 
-App.PRODUCTS = [
-  {
+App.Product.FIXTURES = [
+  { 
+    id: 1,
     title: 'Flint',
     price: 99,
     description: 'Flint is a thing',
     isOnSale: true,
     image: 'images/flint.jpg'
   },
-  {
+  { 
+    id: 2,
     title: 'Kindling',
     price: 249,
     description: 'Easily one of the best things',
